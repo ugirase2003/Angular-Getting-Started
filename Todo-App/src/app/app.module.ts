e8e53453561d5e31b76d42e07a69eeccb72da9ed
Ujjwal Girase <ugirase2003@gmail.com>
2023-09-07 00:21:17 +0530

    components created

--- Todo-App/src/app/app.module.ts
+++ Todo-App/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './mycomponent/todos/todos.component';
+import { AddTodoComponent } from './mycomponent/add-todo/add-todo.component';
+import { TodoItemComponent } from './mycomponent/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    AddTodoComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
