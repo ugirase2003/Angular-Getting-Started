3f6cb5cc45e711cc96208c214e767264eebe783a
Ujjwal Girase <ugirase2003@gmail.com>
2023-09-07 00:58:04 +0530

    todo item style and add-todo component's logic is done

--- Todo-App/src/app/Todo.ts
+++ Todo-App/src/app/Todo.ts
@@ -1,6 +1,6 @@
 export class Todo{
     // if  "strict": false  it is true in tsconfig.json then here u will get errror of initialization
-    sno:number
+
     title:string
     desc:string
     active:boolean
--- Todo-App/src/app/mycomponent/add-todo/add-todo.component.ts
+++ Todo-App/src/app/mycomponent/add-todo/add-todo.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,Output,EventEmitter } from '@angular/core';
+import { Todo } from 'src/app/Todo';
 
 @Component({
   selector: 'app-add-todo',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-todo.component.css']
 })
 export class AddTodoComponent {
-
+  title:string;
+  desc:string;
+  @Output() todoAdd:EventEmitter<Todo>=new EventEmitter();
+  constructor(){}
+  onSubmit(){
+    const todo={
+    
+      title:this.title,
+      desc:this.desc,
+      active:true
+    }
+    this.todoAdd.emit(todo)
+  }
 }
