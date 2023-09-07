c4eb31bcfd311d0a6417fd4ea199a421597bc28f
Ujjwal Girase <ugirase2003@gmail.com>
2023-09-07 10:24:42 +0530

    routing implemented

--- Todo-App/src/app/app-routing.module.ts
+++ Todo-App/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from './mycomponent/todos/todos.component';
+import { AboutComponent } from './mycomponent/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // '' (blank quotes)it will redirect to  '/' i.e homepage 
+  {path:'',component:TodosComponent},
+  {path:'about',component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
